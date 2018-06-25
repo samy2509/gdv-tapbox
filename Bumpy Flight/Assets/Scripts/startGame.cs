c5b06900625b1df0608913d4b3f181b8eb009cbb
Kevin Schipper <kevin.schipper@informatik.hs-fulda.de>
2018-06-25 15:05:54 +0200

    Hauptmenü hinzugefügt

---
+++ Bumpy Flight/Assets/Scripts/startGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour {
+
+	public void StartGame() {
+		Debug.Log("Loading Level 1...");
+		// SceneManager.LoadScene("");
+	}
+
+	public void QuitGame() {
+		Debug.Log("QUITTED");
+		Application.Quit();
+	}
+}
