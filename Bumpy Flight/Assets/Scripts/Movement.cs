8797aa5b2f1411da51839c6321f95150bd3325a5
AKuppe <40357220+AKuppe@users.noreply.github.com>
2018-07-01 13:13:24 +0200

    Add files via upload

---
+++ Bumpy Flight/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float mSpeed = 10.0f;
+    private float gravity = 14.0f;
+    private float jumpForce = 14.0f;
+    private Vector3 moveDi = Vector3.zero;
+
+    void Start () {
+        mSpeed = 7.0f;
+	}
+
+	void Update () {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            //Bewegung nach rechts, links
+            moveDi = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDi = transform.TransformDirection(moveDi);
+            moveDi *= mSpeed;
+
+            if (Input.GetButtonDown("Jump")) {
+                moveDi.y = jumpForce;
+            }
+            
+        }
+        //fallen
+        moveDi.y -= gravity * Time.deltaTime;
+        controller.Move(moveDi * Time.deltaTime);
+    }
+
+}
