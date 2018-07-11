ee293356f02ba984343da6c91323bcef39ef7689
AKuppe <40357220+AKuppe@users.noreply.github.com>
2018-07-11 21:17:17 +0200

    Leben der Figur
    Leben und was passiert bei GameOver

---
+++ Bumpy Flight/Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public float currentHealth = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ApplyDamage(float damage) {
+
+        if (currentHealth > 0) {
+            currentHealth -= damage;
+            if (currentHealth < 0)
+            {
+                currentHealth = 0;
+            }
+            if (currentHealth == 0)
+            {
+                //GameOver neustart der scene
+                RestartScene();
+            }
+        }
+        
+
+    }
+
+    //Restartet das Level
+    void RestartScene()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
