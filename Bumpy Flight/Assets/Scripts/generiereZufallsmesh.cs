5a200604bb78a8af4e5b74439db76934baacfa85
Lukas Bröning <lukas.broening@ai.hs-fulda.de>
2018-07-12 10:12:54 +0200

    Low Poly implementiert und kleinere Korrekturen
    Änderungen in "generiereZufallsmesh". Script, um Zufallsmesh zu generieren. Low-Poly-Look ist nun implementiert.
    Mesh wird noch auf Boden, Wände und Decke erweitert.

--- Bumpy Flight/Assets/Scripts/generiereZufallsmesh.cs
+++ Bumpy Flight/Assets/Scripts/generiereZufallsmesh.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 
 public class generiereZufallsmesh : MonoBehaviour
 {
-	public  int 		laenge, breite	= 0;            // Laenge und Breite des Zufallsmeshes
-	private Mesh 		mesh;                           // Mesh
-	private int			meshGroesse		= 0;            // Meshgroesse
-    private Vector3[] 	verts;                          // Vertices
-	private Vector2[]   uvCoords;                       // UV Koordinaten
+    public  int         laenge, breite  = 0;            // Laenge und Breite des Zufallsmeshes
+    private Mesh        mesh;                           // Mesh
+    private int         meshGroesse     = 0;            // Meshgroesse
+    private Vector3[]   verts;                          // Vertices
+    private Vector2[]   uvCoords;                       // UV Koordinaten
     private int[]       tris;                           // Dreiecke (Triangles)
     
-	// Awake is called when the script instance is being loaded
+    // Awake is called when the script instance is being loaded
     private void Awake()
     {
         laenge  = Random.Range(10, 20);
@@ -31,7 +31,7 @@ public class generiereZufallsmesh : MonoBehaviour
     private void Generiere()
     {
         GetComponent<MeshFilter>().mesh = mesh;
-		meshGroesse                     = (laenge + 1) * (breite + 1);
+        meshGroesse                     = (laenge + 1) * (breite + 1);
         verts                           = new Vector3[meshGroesse];
         uvCoords                        = new Vector2[meshGroesse];
 
@@ -40,7 +40,7 @@ public class generiereZufallsmesh : MonoBehaviour
         {
             for (int x = 0; x <= laenge; x++)
             {
-                verts[i]    = new Vector3(x, y);
+                verts[i]    = new Vector3(x, y, Random.Range(0.0f, 1.0f));
                 uvCoords[i] = new Vector2((float)x / laenge, (float)y / breite);
 
                 i++;
