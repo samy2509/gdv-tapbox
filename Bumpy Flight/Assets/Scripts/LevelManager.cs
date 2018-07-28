53575eeacb97cad2405fb1022302919a0abc4816
AKuppe <40357220+AKuppe@users.noreply.github.com>
2018-07-28 14:19:08 +0200

    Checkpoint,enemy,levelManger und respawn
    health und damage collider sollten entfernt werden aus dem script. habe mich für eine andere variante der lebensanzeige entschieden.

---
+++ Bumpy Flight/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+    public GameObject player;
+    public GameObject currentCheckpoint;
+    public int health;
+    public Text healthText;
+
+	// Use this for initialization
+	void Start () {
+        healthText.text = health.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RespawnPlayer() {
+        //leben abziehen
+        health = health - 1;
+        //lebensanzeige aktualisieren
+        healthText.text = health.ToString();
+        //überprüfen ob spieler leben hat
+        if (health > 0)
+        {
+            //wenn ja -> zurück zum checkpoint
+            player.transform.position = currentCheckpoint.transform.position;
+
+        }
+        else {
+            //wenn nein -> spielende
+            Debug.Log("Game Over");
+            Time.timeScale = 0.0f;
+        }
+
+    }
+}
---
+++ Bumpy Flight/Assets/Scripts/eenemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eenemy : MonoBehaviour {
+
+    public LevelManager levelManager;
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            levelManager.RespawnPlayer();
+        }
+    }
+}
