f0a2a7deb0674c6724c08c6adf9e35995af3b540
AKuppe <40357220+AKuppe@users.noreply.github.com>
2018-08-09 17:11:35 +0200

    Laufen gefixt
    nurnoich nach rechts und links laufen + springen

--- Bumpy Flight/Assets/Scripts/PlayerMovement.cs
+++ Bumpy Flight/Assets/Scripts/PlayerMovement.cs
@@ -3,31 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+    CharacterController controller;
+    Vector3 moveDirection = Vector3.zero;
     public float mSpeed = 10.0f;
     private float gravity = 44.0f;
     private float jumpForce = 24.0f;
-    private Vector3 moveDi = Vector3.zero;
+    private float velocity = 0;
+    private bool inputJump;
 
     void Start () {
+        controller = gameObject.GetComponent<CharacterController>();
         mSpeed = 7.0f;
 	}
 
 	void Update () {
-        CharacterController controller = gameObject.GetComponent<CharacterController>();
-        if (controller.isGrounded) {
-            //Bewegung nach rechts, links
-            moveDi = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDi = transform.TransformDirection(moveDi);
-            moveDi *= mSpeed;
-
-            if (Input.GetButtonDown("Jump")) {
-                moveDi.y = jumpForce;
-            }
-            
+        InputCheck();
+        Move();
+        //SetAnimation();
+    }
+    void InputCheck() {
+        velocity = Input.GetAxis("Horizontal") * mSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inputJump = true;
         }
-        //fallen
-        moveDi.y -= gravity * Time.deltaTime;
-        controller.Move(moveDi * Time.deltaTime);
+        else {
+            inputJump = false;
+        }
+    }
+    void Move() {
+        if (controller.isGrounded) {
+            if (inputJump) {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.x = velocity;
+        moveDirection.y -= gravity * Time.deltaTime; 
+        controller.Move(moveDirection * Time.deltaTime);
+        
+        
     }
 
+   /* void SetAnimation() {
+    }Später für animatin beim springen 
+    */
 }
