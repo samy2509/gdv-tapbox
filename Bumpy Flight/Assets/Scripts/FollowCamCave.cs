9242cd56aad6dbd3db05c3237203a1a9ee9ead85
samy2509 <34767730+samy2509@users.noreply.github.com>
2018-08-17 00:34:33 +0200

    Camera & Animation in cave

---
+++ Bumpy Flight/Assets/Scripts/FollowCamCave.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowCamCave : MonoBehaviour {
+
+	public Transform target;
+
+	void Update () {
+		Camera.main.fieldOfView = 45;
+	}
+
+	void LateUpdate () {
+		Vector3 offset = new Vector3(0f, 6f, -12f);
+		Vector3 softCam = Vector3.Lerp(transform.position, target.position + offset, 0.25f);
+		transform.position = softCam;
+
+		transform.LookAt(target);
+	}
+}
