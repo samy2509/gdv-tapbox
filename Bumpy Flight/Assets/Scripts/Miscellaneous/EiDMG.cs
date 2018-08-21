3a14e79143cba0ea4c3b8542438d436c8c255d1f
AKuppe <40357220+AKuppe@users.noreply.github.com>
2018-08-21 19:15:25 +0200

    Ei verschwindet
    ei lebenszeit kurz und verschwindet immer , prefabs muss das script noch hinzu gefügt werden pls @kevin

---
+++ Bumpy Flight/Assets/Scripts/Miscellaneous/EiDMG.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EiDMG : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("eiStop"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
