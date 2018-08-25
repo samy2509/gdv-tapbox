3c890425ecb743190bc41c0001a7a9246ebe098d
Lucas <lucas@it-media-fulda.de>
2018-08-25 03:13:20 +0200

    Improvements on animations

--- Bumpy Flight/Assets/Scripts/Miscellaneous/ChickAnim.cs
+++ Bumpy Flight/Assets/Scripts/Miscellaneous/ChickAnim.cs
@@ -7,45 +7,53 @@ public class ChickAnim : MonoBehaviour {
 	public Animator anim;
 	CharacterController controller;
 
-	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator>();
-		controller      = gameObject.GetComponent<CharacterController>();
+		anim 			= 			GetComponent<Animator>();
+		controller      = 			gameObject.GetComponent<CharacterController>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-
+		// Vorwärts laufen
 		if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
 			anim.Play("Walk", -1, 0f);
 		}
+		// Vorwärts laufen Ende
 		if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) {
 			anim.Play("Idle_A", -1, 0f);
 		}
 
 
-		if(Input.GetKeyDown("space") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-			anim.Play("Fly", -1, 0f);
-
-			if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-				anim.Play("Walk", -1, 0f);
-			}
-		}
-
-
+		// Ei abwerfen
 		if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
 			anim.Play("Roll", -1, 0f);
 		}
+		// Ei abwerfen Ende
 		if(Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) {
 			anim.Play("Idle_A", -1, 0f);
 		}
 
 
+		// Zurück laufen
 		if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
-			anim.Play("Clicked", -1, 0f);
+			anim.Play("Walk", -1, 0f);
 		}
+		// Zurück laufen Ende
 		if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
 			anim.Play("Idle_A", -1, 0f);
+		}
+
+
+		// Springen
+		if(Input.GetKeyDown("space") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+			anim.Play("Fly", -1, 0f);
+		}
+		// Springen Ende
+		/*if(Input.GetKeyUp("space") || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
+			anim.Play("Idle_A", -1, 0f);
+		}*/
+
+		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && Input.GetKeyUp("space") || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
+			anim.Play("Walk", -1, 0f);
 		}
 	}
 }
